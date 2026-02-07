98e046a10ef043d422173c47f734b20bb5f38d17
benaleobayu <benaleobayu@gmail.com>
2026-02-07 10:57:20 +0000

    add : navbar init

---
+++ components/elements/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="sticky top-0 left-0 w-full bg-white shadow h-14 flex justify-between items-center">
+                navbar
+            </nav>
+            </div>
+    )
+}
