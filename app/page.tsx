fac25435b9e8a993a98b901df01f5df249d06871
benaleobayu <benaleobayu@gmail.com>
2026-02-08 03:38:36 +0000

    upd : page landing hero init

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
+import LandingHero from "@/components/elements/landing-hero";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Home",
-  description: "This is a main page"
-}
+  description: "This is a main page",
+};
 
 export default function Page() {
   return (
     <main className="min-h-[150vh] bg-slate-200">
-      test
-      </main>
+      <LandingHero />
+    </main>
   );
 }
---
+++ components/elements/landing-hero.tsx
@@ -0,0 +1,7 @@
+export default function LandingHero(){
+    return (
+        <section>
+            this is hero section
+        </section>
+    )
+}
